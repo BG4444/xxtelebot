727a0657c825f09f51f43175713829e2921da283
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-27 18:20:20 +0000

    Deploy StefanoBelli/xxtelebot to github.com/StefanoBelli/xxtelebot.git:gh-pages

---
+++ search/variables_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['imtypephoto',['imTypePhoto',['../namespacetgbot_1_1methods_1_1types.html#a2307d835936a963861be011823369b7b',1,'tgbot::methods::types']]],
+  ['imtypevideo',['imTypeVideo',['../namespacetgbot_1_1methods_1_1types.html#a3badf240662a67190b6fa7f047d64557',1,'tgbot::methods::types']]]
+];
